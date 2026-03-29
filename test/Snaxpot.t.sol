024f47cc6497cea4920c5d1de4f1a45a9f412e47
ProblematicP <problematicp@cc.snxdao.io>
2026-03-30 10:52:13 +1100

    open and cloase epoch tests

--- test/Snaxpot.t.sol
+++ test/Snaxpot.t.sol
@@ -8,6 +8,7 @@ import {Snaxpot} from "../src/Snaxpot.sol";
 import {ISnaxpot} from "../src/interfaces/ISnaxpot.sol";
 import {IJackpotClaimer} from "../src/interfaces/IJackpotClaimer.sol";
 import {IVRFCoordinatorV2Plus} from "../src/chainlink/interfaces/IVRFCoordinatorV2Plus.sol";
+import {VRFV2PlusClient} from "../src/chainlink/libraries/VRFV2PlusClient.sol";
 
 import {MockERC20} from "./mocks/MockERC20.sol";
 
@@ -39,10 +40,16 @@ contract SnaxpotTest is Test {
         vrfCoordinator = makeAddr("vrfCoordinator");
         jackpotClaimer = makeAddr("jackpotClaimer");
 
-        vm.mockCall(jackpotClaimer, abi.encodeWithSelector(IJackpotClaimer.credit.selector), abi.encode());
+        vm.mockCall(
+            jackpotClaimer,
+            abi.encodeWithSelector(IJackpotClaimer.credit.selector),
+            abi.encode()
+        );
         vm.mockCall(
             vrfCoordinator,
-            abi.encodeWithSelector(IVRFCoordinatorV2Plus.requestRandomWords.selector),
+            abi.encodeWithSelector(
+                IVRFCoordinatorV2Plus.requestRandomWords.selector
+            ),
             abi.encode(VRF_REQUEST_ID)
         );
 
@@ -77,11 +84,15 @@ contract SnaxpotTest is Test {
     //   DRAW_B: words [100, 200, 300, 400, 500, 7] → balls [5, 9, 13, 17, 21], snaxBall 3
     //
     /// @dev Mirrors Snaxpot._deriveBalls so tests can predict winning numbers from VRF words.
-    function _deriveBalls(uint256[] memory randomWords) internal pure returns (uint8[5] memory balls, uint8 snaxBall) {
+    function _deriveBalls(
+        uint256[] memory randomWords
+    ) internal pure returns (uint8[5] memory balls, uint8 snaxBall) {
         uint256 usedMask;
         uint8 count;
         for (uint8 i = 0; count < 5; i++) {
-            uint256 rand = i < 5 ? randomWords[i] : uint256(keccak256(abi.encodePacked(randomWords[i - 1], i)));
+            uint256 rand = i < 5
+                ? randomWords[i]
+                : uint256(keccak256(abi.encodePacked(randomWords[i - 1], i)));
             uint8 ball = uint8((rand % 32) + 1);
             uint256 bit = uint256(1) << ball;
             if (usedMask & bit == 0) {
@@ -112,5 +123,93 @@ contract SnaxpotTest is Test {
         assertFalse(snaxpot.paused());
         assertTrue(snaxpot.hasRole(snaxpot.DEFAULT_ADMIN_ROLE(), admin));
         assertTrue(snaxpot.hasRole(snaxpot.OPERATOR_ROLE(), operator));
+    }
+
+    function test_openEpoch_happyPath() public {
+        vm.expectCall(
+            vrfCoordinator,
+            abi.encodeCall(
+                IVRFCoordinatorV2Plus.requestRandomWords,
+                (VRFV2PlusClient.RandomWordsRequest({
+                    keyHash: VRF_KEY_HASH,
+                    subId: VRF_SUB_ID,
+                    requestConfirmations: VRF_CONFIRMATIONS,
+                    callbackGasLimit: VRF_CALLBACK_GAS,
+                    numWords: 1,
+                    extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}))
+                }))
+            )
+        );
+
+        vm.prank(operator);
+        snaxpot.openEpoch();
+
+        assertEq(snaxpot.currentEpochId(), 1);
+
+        ISnaxpot.EpochData memory epoch = snaxpot.getEpoch(1);
+        assertEq(uint8(epoch.state), uint8(ISnaxpot.EpochState.OPEN));
+        assertEq(epoch.startTimestamp, block.timestamp);
+    }
+
+    function test_openEpoch_whenNotOperator_reverts() public {
+        vm.prank(alice);
+        vm.expectRevert();
+        snaxpot.openEpoch();
+    }
+
+    function test_openEpoch_whenPaused_reverts() public {
+        vm.prank(admin);
+        snaxpot.pause();
+
+        vm.prank(operator);
+        vm.expectRevert(ISnaxpot.ContractPaused.selector);
+        snaxpot.openEpoch();
+    }
+
+    function test_closeEpoch_happyPath() public {
+        vm.startPrank(operator);
+        snaxpot.openEpoch();
+        snaxpot.closeEpoch(1);
+        vm.stopPrank();
+
+        ISnaxpot.EpochData memory epoch = snaxpot.getEpoch(1);
+        assertEq(uint8(epoch.state), uint8(ISnaxpot.EpochState.CLOSED));
+        assertEq(epoch.closeTimestamp, block.timestamp);
+        assertEq(epoch.jackpotAmount, 0);
+        assertEq(snaxpot.currentJackpot(), 0);
+    }
+
+    function test_closeEpoch_whenNotOperator_reverts() public {
+        vm.prank(operator);
+        snaxpot.openEpoch();
+
+        vm.prank(alice);
+        vm.expectRevert();
+        snaxpot.closeEpoch(1);
+    }
+
+    function test_closeEpoch_whenPaused_reverts() public {
+        vm.prank(operator);
+        snaxpot.openEpoch();
+
+        vm.prank(admin);
+        snaxpot.pause();
+
+        vm.prank(operator);
+        vm.expectRevert(ISnaxpot.ContractPaused.selector);
+        snaxpot.closeEpoch(1);
+    }
+
+    function test_closeEpoch_whenEpochNotOpen_reverts() public {
+        vm.prank(operator);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ISnaxpot.InvalidEpochState.selector,
+                1,
+                ISnaxpot.EpochState.NONE,
+                ISnaxpot.EpochState.OPEN
+            )
+        );
+        snaxpot.closeEpoch(1);
     }
 }
